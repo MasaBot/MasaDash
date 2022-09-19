da0e18974153750ced28b0e0a7bfba49e46582f2
Tembero <silver.teemu@gmail.com>
2022-09-19 22:53:09 +0300

    theme testing

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,25 +1,38 @@
 import { useState } from "react";
 
 const themes = {
-    DARK: {
+    DARK_AS_DEFAULT: {
         "--background-dark": "#030618",
         "--background-light": "#060a1f",
         "--text-color": "white",
         "--highlight": "#Bc1930",
-        "--highlight-dark": "#991627"
+        "--highlight-dark": "#991627",
+        "--function-disabled-bg": "#464646",
+        "--function-disabled-fg": "#d3d3d3"
     },
-    GREY: {
-        "--background-dark": "#1d1d1d",
-        "--background-light": "#525252",
+    DREAMING: {
+        "--background-dark": "#a2d2ff",
+        "--background-light": "#bde0fe",
+        "--text-color": "#393D3F",
+        "--highlight": "#ffc8dd",
+        "--highlight-dark": "#ffafcc",
+        "--function-disabled-bg": "#c2c2c2",
+        "--function-disabled-fg": "#212121"
+    },
+    CAMO: {
+        "--background-dark": "black",
+        "--background-light": "#0e0e0e",
         "--text-color": "white",
-        "--highlight": "#242424",
-        "--highlight-dark": "#000"
-    },
+        "--highlight": "#001703",
+        "--highlight-dark": "#000b01",
+        "--function-disabled-bg": "#464646",
+        "--function-disabled-fg": "#d3d3d3"
+    }
 }
 
 export default function SettingsPage() {
 
-    const [currentTheme, setCurrentTheme] = useState<keyof typeof themes>("DARK");
+    const [currentTheme, setCurrentTheme] = useState<keyof typeof themes>("DARK_AS_DEFAULT");
 
     function themeChange(e: React.ChangeEvent<HTMLInputElement>) {
         setCurrentTheme(e.target.value as keyof typeof themes);
@@ -45,7 +58,15 @@ export default function SettingsPage() {
         }}>
             {
                 Object.entries(themes).map(([key, value]) => {
-                    return <input key={key} type="radio" value={key} onChange={themeChange} checked={isChecked(key)} />
+                    return (
+                        <>
+                            <label>
+                                {key.split("_").map(str => str.substring(0, 1) + str.substring(1).toLowerCase()).join(" ")}
+                            </label>
+                            <input key={key} type="radio" value={key} onChange={themeChange} checked={isChecked(key)} />
+                            <hr />
+                        </>
+                    )
                 })
             }
             <button type="submit">Save</button>
