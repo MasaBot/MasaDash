3b1f6f853ed13d9fa7806d0a452bbd9dacc03f97
ittuki <ittuki@kukkula.local>
2022-09-27 15:42:29 +0300

    Updated API routes

--- src/utils/changeServerName.ts
+++ src/utils/changeServerName.ts
@@ -1,10 +1,10 @@
 export default async function changeServerName(server: string, newName: string) {
-  await fetch(`http://localhost:3000/api/v1/server/change-name`, {
+  await fetch(`http://localhost:3000/api/v1/server/${server}/change-name`, {
     method: "post",
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({newName, server})
+    body: JSON.stringify({ newName })
   })
 }
--- src/utils/serverControl.ts
+++ src/utils/serverControl.ts
@@ -1,10 +1,10 @@
 export async function startServer(tag: string) {
-    await fetch(`http://localhost:3000/api/v1/server/start/${tag}`, {
+    await fetch(`http://localhost:3000/api/v1/server/${tag}/start/`, {
         method: "post"
     });
 }
 export async function stopServer(tag: string) {
-    await fetch(`http://localhost:3000/api/v1/server/stop/${tag}`, {
+    await fetch(`http://localhost:3000/api/v1/server/${tag}/stop/`, {
         method: "post"
     });
 }
